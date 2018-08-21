a9ed63df59bdf9822216e8fe0c3e005bdebe8ef9
Tsunemichi Harada <fox10225fox@gmail.com>
2018-08-21 20:17:20 +0900

    go home

--- src/App.jsx
+++ src/App.jsx
@@ -50,14 +50,14 @@ class BooksApp extends React.Component {
                   <div className="bookshelf-books">
                     <ol className="books-grid">
                     <li>
-                        <Book 
+                        <Book
                           title="To Kill a Mockingbird" 
                           author="Harper Lee" 
                           cover="http://books.google.com/books/content?id=PGR2AwAAQBAJ&amp;printsec=frontcover&amp;img=1&amp;zoom=1&amp;imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&amp;source=gbs_api"
                         />
                       </li>
                       <li>
-                        <Book 
+                        <Book
                           title="Ender's Game" 
                           author="Orson Scott Card" 
                           cover="http://books.google.com/books/content?id=yDtCuFHXbAYC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72RRiTR6U5OUg3IY_LpHTL2NztVWAuZYNFE8dUuC0VlYabeyegLzpAnDPeWxE6RHi0C2ehrR9Gv20LH2dtjpbcUcs8YnH5VCCAH0Y2ICaKOTvrZTCObQbsfp4UbDqQyGISCZfGN&source=gbs_api"
