7c5ebf00508f1f205a36fd688b7607e47ae37a8b
Tsunemichi Harada <fox10225fox@gmail.com>
2018-08-22 06:12:51 +0900

    fixes #5

---
+++ src/components/BookShelf.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import Book from './Book'
+
+const BookShelf = props => {
+  const {books, title} = props
+
+  return (
+    <div className="bookshelf">
+    <h2 className="bookshelf-title">{title}</h2>
+    <div className="bookshelf-books">
+      <ol className="books-grid">
+      {books.map(book => (
+        <li>
+          <Book
+            title={book.title}
+            author={book.author}
+            cover={book.cover}
+          />
+        </li>
+      ))}
+      </ol>
+    </div>
+  </div>
+  )
+}
+
+export default BookShelf
