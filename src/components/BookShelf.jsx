2f1afd8eb4da59e42b2a8a43a91953eb9e6c64e3
Tsunemichi Harada <fox10225fox@gmail.com>
2018-08-22 11:27:29 +0900

    booksの定義をstateにした

--- src/components/BookShelf.jsx
+++ src/components/BookShelf.jsx
@@ -2,25 +2,25 @@ import React from 'react'
 import Book from './Book'
 
 const BookShelf = props => {
-  const {books, title} = props
+  const { books, title } = props
 
   return (
     <div className="bookshelf">
-    <h2 className="bookshelf-title">{title}</h2>
-    <div className="bookshelf-books">
-      <ol className="books-grid">
-      {books.map(book => (
-        <li>
-          <Book
-            title={book.title}
-            author={book.author}
-            cover={book.cover}
-          />
-        </li>
-      ))}
-      </ol>
+      <h2 className="bookshelf-title">{title}</h2>
+      <div className="bookshelf-books">
+        <ol className="books-grid">
+          {books.map(book => (
+            <li>
+              <Book
+                title={book.title}
+                author={book.author}
+                cover={book.cover}
+              />
+            </li>
+          ))}
+        </ol>
+      </div>
     </div>
-  </div>
   )
 }
 
